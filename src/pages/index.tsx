ba3de86c8cc9b4c36c522d2c5b21be85e719c959
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-06-08 10:13:43 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,35 @@
 //import Image from "next/image";
+const nome="Ronaldo"
+let telefone = '98986078012'
+let email ="eletrotecnica.ronaldo@gmail.com"
+function retorNome(){
+  return nome
+}
+const nol=()=>{
+  return email
+}
 
+function Topo(){
+  return(
+    <div className='flex justify-between items-center bg-zinc-300'>
+      <div>logo</div>
+      <div> {nol()} </div>
+      <div> {nome} </div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <main>
+      <Topo/>
+      <div>-------------------</div>
+      
       <div>Ola</div>
       <div>Esse é meu curso</div>
-      <div>Meu nome é Ronaldo</div>
+      <div>Meu nome é {retorNome()}</div>
       <div>Tenho 43 Anos</div>
+      <div> {nol()}</div>
     </main>
   );
 }
