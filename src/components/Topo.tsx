01ca451f3c897f0e6685352f45a04009299dae7f
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-07-02 09:46:32 -0300

    atualização

---
+++ src/components/Topo.tsx
@@ -0,0 +1,9 @@
+export default function Topo(){
+    return(
+         <div className='flex justify-between items-center bg-zinc-300 '>
+        <div>logo</div>
+        <div> curso Java</div>
+        <div> Curso Rect </div>
+       </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,35 @@
 //import Image from "next/image";
 const nome="Ronaldo"
-let telefone = '98986078012'
-let email ="eletrotecnica.ronaldo@gmail.com"
+const telefone = '98986078012'
+const email ="eletrotecnica.ronaldo@gmail.com"
 function retorNome(){
-  return nome
+  return nome  
 }
 const nol=()=>{
   return email
 }
 
-function Topo(){
+/*function Topo(){
   return(
-    <div className='flex justify-between items-center bg-zinc-300'>
+    <div className='flex justify-between items-center bg-zinc-300 '>
       <div>logo</div>
       <div> {nol()} </div>
       <div> {nome} </div>
     </div>
   )
 }
-
+*/
 export default function Home() {
   return (
     <main>
-      <Topo/>
+      
       <div>-------------------</div>
       
       <div>Ola</div>
       <div>Esse é meu curso</div>
       <div>Meu nome é {retorNome()}</div>
       <div>Tenho 43 Anos</div>
+      <div>{telefone}</div>
       <div> {nol()}</div>
     </main>
   );
